c13c7bbdaebd0874d26b9a69a10ca05b780878f0
Ahmed Gamal <ahmed.gamal@kickstartinteractive.net>
2022-09-06 14:42:50 +0200

    add valiodation and styling

--- src/app/components/users/update.component.ts
+++ src/app/components/users/update.component.ts
@@ -1,7 +1,8 @@
-import {  Component, EventEmitter, Input, OnInit, Output  } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output  } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UsersService } from './users.service';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { User } from './user.interface';
 
 @Component({
   selector: 'app-users',
@@ -10,17 +11,21 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 })
 export class UsersUpdateComponent implements OnInit {
     public userForm!: FormGroup;
-    id: any;
-    users: any;
-    submitted = false;
-    action ='edit'
+    userId: any;
+    users!: User;
+    submitted:boolean = false;
+    action: string ='edit';
+
   constructor(private UsersService: UsersService,private route: ActivatedRoute,
     private router: Router,private fb: FormBuilder,) { 
-    
+  }
+
+  ngOnInit(): void {
+    this.userId = this.route.snapshot.params;
+    this.getUser(this.userId.userId);
   }
 
   getUser(id: number) {
-    
     this.UsersService.getUser(id).subscribe(res => {
         this.users = res.data; 
         const data = res.data;
@@ -55,21 +60,44 @@ export class UsersUpdateComponent implements OnInit {
         // }); 
       }) 
   }
+
   onSubmit() {
     console.log('submit');
     if (this.userForm.dirty && this.userForm.valid) {
       console.log('userForm is valid >>', this.userForm.value);
+    } else {
+      this.userForm.markAsTouched();
+      this.userForm.markAsDirty();
     }
 
     // stop here if form is invalid
     // if (this.form.invalid) {
     //     return;
     // }
-}
-  ngOnInit(): void {
-    this.id = this.route.snapshot.params;
-    this.getUser(this.id.userId);   
-    
+  }
+
+  get birthday() {
+    return this.userForm.get('birthday');
+  }
+
+  get details() {
+    return this.userForm.get('details');
+  }
+
+  get email() {
+    return this.userForm.get('email');
+  }
+
+  get phone() {
+    return this.userForm.get('phone');
+  }
+
+  get type() {
+    return this.userForm.get('type');
+  }
+
+  get userName() {
+    return this.userForm.get('userName');
   }
   
 }
---
+++ src/app/components/users/user.interface.ts
@@ -0,0 +1,8 @@
+export interface User {
+    birthday: Date;
+    details: string;
+    email: string;
+    phone: string;
+    type: string;
+    userName: string;
+}
